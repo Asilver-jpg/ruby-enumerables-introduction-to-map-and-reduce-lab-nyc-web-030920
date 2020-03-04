d40d39a4a9d14118c0d55ce0bdae878310dc5826
Alexander Silver <asilver7493@gmail.com>
2020-03-04 23:16:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,29 @@
-def map_to_negativize(arr)
+def map_to_negativize(src)
+  array=[]
+  src.each |ele|{ }
+  
+end
+
+def map_to_no_change(src)
+  
   
   
 end
 
-def
+def map_to_double(src)
+  
+  
+  
+  
+end
+def map_to_square(src)
+end
+def reduce_to_total (src, start)
+end
+def  reduce_to_all_true(src)
+end
+def reduce_to_any_true(src)
+end
+
+
+
