14767b197607c28604db6e64a242970402fbd562
Alexander Silver <asilver7493@gmail.com>
2020-03-04 23:18:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,21 +6,29 @@ end
 
 def map_to_no_change(src)
   array=[]
-  src.each {|n| array << n*-1 }
+  src.each {|n| array << n }
   array
   
   
 end
 
 def map_to_double(src)
-  
+  array=[]
+  src.each {|n| array << n*2 }
+  array
   
   
   
 end
 def map_to_square(src)
+  array=[]
+  src.each {|n| array << n*n}
+  array
 end
 def reduce_to_total (src, start)
+  total=0
+  src.each {|n| total += n}
+  total
 end
 def  reduce_to_all_true(src)
 end
